94dad81dcca7edf26c2e6e385a755ae519f0ee66
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-08 20:03:52 +0530

    Update streamlit_app.py
    skills updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,17 @@ col1,col2 = st.columns([0.3,0.7])
 
 with col1:
   st.title("Hey, I'm Naveen Kumar Konam")
-  st.header("Python Developer")
+  st.header("Data Engineer")
+
+  st.markdown("#### Skills")
+  st.write("Python")
+  st.write("SQL")
+  st.write("Hadoop")
+  
 
 with col2:
-  st.header("I am analyst turned Developer")
+  st.markdown("## About Me")
   st.write("These are my projects")
+
+  st.markdown("## Projects")
 
