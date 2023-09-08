f45f4dc910413cbdc839427acf132b1913452600
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-08 19:59:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import streamlit as st
 col1,col2 = st.columns(2)
 
 with col1:
-  st.header("Hey, I'm Naveen Kumar Konam")
-  st.title("Python Developer")
+  st.title("Hey, I'm Naveen Kumar Konam")
+  st.header("Python Developer")
 
 with col2:
-  st.title("I am analyst turned Developer")
+  st.header("I am analyst turned Developer")
   st.write("These are my projects")
 
