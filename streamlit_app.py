c37195feba53d524a84543cf74f56f03e7e65f10
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-08 19:56:34 +0530

    Update streamlit_app.py
    Initial code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
 # Streamlit app for Portfolio
+import streamlit
+
+col1,col2 = st.columns(2)
+
+with col1:
+  st.header("Hey, I'm Naveen Kumar Konam")
+  st.title("Python Developer")
+
+with col2:
+  st.title("I am analyst turned Developer")
+  st.write("These are my projects")
+
