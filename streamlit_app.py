4f5c40189519caa4d3293f53ba0b28214f952a13
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-08 20:09:08 +0530

    Update streamlit_app.py
    updated a project

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,10 @@ with col1:
 
 with col2:
   st.markdown("## About Me")
-  st.write("These are my projects")
+  st.write("Myself an aspiring data engineer, interested in building paltforms which can help the customers achieve their tasks effortlessly and efficiently")
 
   st.markdown("## Projects")
+  st.markdown("### Table Crawler")
+  st.write("Table Crawler is a simple project to scrape the tables from webpages without writing code")
+  st.markdown("![Table Crawler]https://tablecrawler.streamlit.app/")
 
